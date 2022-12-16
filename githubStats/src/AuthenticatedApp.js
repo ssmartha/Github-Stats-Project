a1c5fb74f0c9bdd4f7d039abe205148072638966
Martha Huaman <ms.huamann@gmail.com>
2022-12-15 21:30:19 -0500

    ADD: home page with footer(routes, icons & links in AuthenticatedApp.js, made by Carlos & Martha)

--- githubStats/src/AuthenticatedApp.js
+++ githubStats/src/AuthenticatedApp.js
@@ -9,13 +9,62 @@
 //   getFavorites,
 //   removeFavorite,
 // } from "./services/favorites-service";
+import React from "react";
+import {
+  BrowserRouter, // Para declarar rutas => el padre de todas las rutas
+  Routes, // Se ocupan de listar las rutas independientes.
+  Route, // Para declarar una ruta
+  Link, // Para navegar entre las rutas
+  useParams,
+} from "react-router-dom";
+import { RiSearchFill } from "react-icons/ri";
+import { BsFillStarFill, BsPersonFill } from "react-icons/bs";
 
 function AuthenticatedApp() {
   // const { logout } = useAuth();
+  const Footer = () => (
+  <nav>
+    <Link to="/profile">  {<BsPersonFill/>} </Link>
+    <Link to="/search"> {<RiSearchFill/>} </Link>
+    <Link to="/favorite"> {<BsFillStarFill/>} </Link>
+  </nav>
+  );
+
+  function Profile() {
+    return (
+      <div>
+        <h1>Profile Page</h1>
+      </div>
+    );
+  }
+
+  function Search() {
+    return (
+      <div>
+        <h1>Search Page</h1>
+      </div>
+    );
+  }
+
+  function Favorite() {
+    return (
+      <div>
+        <h1>Favorite Page</h1>
+      </div>
+    );
+  }
 
   return (
     <div>
-      "GITHUB DATA"
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Search />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<Search />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
